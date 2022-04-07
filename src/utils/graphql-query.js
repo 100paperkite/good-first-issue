a3607ab23be72897a340ac577f91141afa5b514d
Baek Jiyeon <100.paperkite@gmail.com>
2022-04-07 23:56:18 +0900

    feat: Add graphql query of github api

---
+++ src/utils/graphql-query.js
@@ -0,0 +1,122 @@
+/**
+ *
+ * @param {number} count - number of issues to fetch
+ * @param {string} name - name of the repository
+ * @param {string} owner - owner of the repository
+ * @param {{string, string}} pageCursor - cursor to fetch next page
+ * @returns
+ */
+export const getAllIssuesByRepository = (name, owner, count = 15, { before, after }) => {
+  return `
+        repository(name: "${name}", owner: "${owner}") {
+          label(name: "good-first-issue") {
+            issues(
+              first: ${count}
+              after: null
+              filterBy: {assignee: null, states: OPEN}
+              orderBy: {field: UPDATED_AT, direction: DESC}
+              ${before ? `before: "${before}"` : ''}
+              ${after ? `after: "${after}"` : ''}
+            ) {
+              totalCount
+              pageInfo {
+                startCursor
+                hasNextPage
+                endCursor
+              }
+              edges {
+                node {
+                  number
+                  title
+                  resourcePath
+                  url
+                  state
+                  labels(first: 5) {
+                    edges {
+                      node {
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `;
+};
+
+/**
+ *
+ * @param {number} count - number of issues to fetch
+ * @param {string} language - language name
+ * @param {string} sortOrder - updated | created | comments
+ * @param {{string, string}} pageCursor - cursor to fetch next page
+ * @returns
+ */
+export const searchIssueByLanguageQuery = (
+  language,
+  sortOrder = 'updated',
+  count = 15,
+  { before, after } = {}
+) => {
+  return `
+  query {
+    search(
+        first: ${count}
+        ${before ? `before: "${before}"` : ''}
+        ${after ? `after: "${after}"` : ''}
+        type: ISSUE
+        query: "label:good-first-issue language:${language} state:open sort:${sortOrder}"
+      ) {
+        pageInfo {
+            endCursor
+            hasNextPage
+            hasPreviousPage
+            startCursor
+          }
+        edges {
+          node {
+            ... on Issue {
+              number
+              title
+              url
+              id
+              publishedAt
+              updatedAt
+              state
+              labels(first: 5) {
+                nodes {
+                  color
+                  name
+                }
+              }
+              assignees(last: 1) {
+                totalCount
+              }
+              repository {
+                id
+                url
+                name
+                owner {
+                  login
+                }
+                description
+                stargazerCount
+                forkCount
+                languages(first: 5, orderBy: {field: SIZE, direction: DESC}) {
+                  edges {
+                    node {
+                      color
+                      name
+                    }
+                    size
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }`;
+};
---
+++ src/utils/graphql.js
@@ -0,0 +1,55 @@
+/**
+ *
+ * @param {string} url
+ * @param {object} headers
+ * @param {string} query
+ * @returns Promise
+ */
+export const fetchGraphQL = async (url, headers, query) => {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      ...headers,
+    },
+    body: JSON.stringify({ query: query }),
+  })
+    .then((res) => res.json())
+    .then((data) => cleanGraphQLResponse(data))
+    .catch((err) => console.log(err));
+};
+
+/**
+ * Remove edges, node and __typename from graphql response
+ *
+ * @param {Object} input - The graphql response
+ * @returns {Object} Clean graphql response
+ */
+export const cleanGraphQLResponse = (input) => {
+  if (!input) return null;
+
+  const isPrimitiveType = (test) => {
+    return test !== Object(test);
+  };
+
+  if (isPrimitiveType(input)) return input;
+
+  const output = {};
+  const isObject = (obj) => {
+    return obj !== null && typeof obj === 'object' && !Array.isArray(obj);
+  };
+
+  Object.keys(input).forEach((key) => {
+    if (input[key] && input[key].edges) {
+      output[key] = input[key].edges.map((edge) => cleanGraphQLResponse(edge.node));
+    } else if (input[key] && input[key].nodes) {
+      output[key] = input[key].nodes.map((node) => cleanGraphQLResponse(node));
+    } else if (isObject(input[key])) {
+      output[key] = cleanGraphQLResponse(input[key]);
+    } else if (key !== '__typename') {
+      output[key] = input[key];
+    }
+  });
+
+  return output;
+};
