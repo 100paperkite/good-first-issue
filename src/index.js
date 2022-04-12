22b2d9fb1c9fe880e0c582580998fe698714ebf2
Baek Jiyeon <100.paperkite@gmail.com>
2022-04-12 21:53:02 +0900

    refactor: Remove unused

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './App.css';
 
-import './index';
-
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
