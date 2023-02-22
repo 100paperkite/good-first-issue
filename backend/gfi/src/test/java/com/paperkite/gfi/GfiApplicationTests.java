0cd0a420b48754617cb7c76f37438f8c4b26f196
Baek Jiyeon <100.paperkite@gmail.com>
2023-02-22 22:36:45 +0900

    feat: Initialize Spring Boot Server

--- backend/gfi/src/test/java/com/paperkite/gfi/GfiApplicationTests.java
+++ backend/gfi/src/test/java/com/paperkite/gfi/GfiApplicationTests.java
@@ -1,13 +1,13 @@
 package com.paperkite.gfi;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GfiApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//
-//    }
+	@Test
+	void contextLoads() {
+	}
 
 }
